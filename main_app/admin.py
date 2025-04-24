0b04095cc882ca38d808a4ebee3de8073a0ab4b3
User Name <user@email.com>
2025-04-23 22:33:40 -0400

    Admin Registering Cat, Fedding, and Toy

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Cat, Feeding, Toy
+
+admin.site.register(Cat)
+
+admin.site.register(Feeding)
+
+admin.site.register(Toy)
 
 # Register your models here.
