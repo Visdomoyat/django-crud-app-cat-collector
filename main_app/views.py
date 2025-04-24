2f0d53f1112c7948ded2b38c55741f1ae9b5c3f1
User Name <user@email.com>
2025-04-23 22:36:18 -0400

    Adding authetication to all the view paths

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat, Toy
+from .forms import FeedingForm
+from django.contrib.auth.views import LoginView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django .contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-def home(request):
-    return HttpResponse('<h1>Hello World🗺️</h1>')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('cat_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+        
+class Home(LoginView):
+    template_name = 'home.html'
+    redirect_authenticated_user = True
+# def home(request):
+#     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+  
+@login_required 
+def cat_index(request):
+    cats = Cat.objects.filter(user=request.user)
+    return render(request, 'cats/index.html', {'cats': cats})
+
+@login_required
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form
+        })
+class CatCreate(LoginRequiredMixin, CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+class CatUpdate(LoginRequiredMixin, UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+    success_url = '/cats/'
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+    
+@login_required
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat_detail', cat_id=cat_id)
 # Create your views here.
+
+class ToyCreate(LoginRequiredMixin, CreateView):
+    model = Toy
+    fields = '__all__'
+    
+class ToyList(LoginRequiredMixin, ListView):
+    model = Toy
+    
+    
+class ToyDetail(LoginRequiredMixin, DetailView):
+    model = Toy
+    
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+    
+class ToyDelete(LoginRequiredMixin, DeleteView):
+    model = Toy
+    success_url = '/toys/'
