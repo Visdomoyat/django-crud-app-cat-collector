8971c9a8fc1fbe58dc4e0dba3cc18e3e82c24732
User Name <user@email.com>
2025-04-23 22:34:46 -0400

    Feat: Cat, Feeding, and Toy models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.views.generic import ListView
+from django.urls import reverse
+from django.contrib.auth.models import User
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('cat_detail', kwargs={'cat_id': self.id})
 # Create your models here.
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+      
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    class Meta:
+        ordering = ['-date']
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+        
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+    
