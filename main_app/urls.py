fda17b4f7b597c2aa72730080f92eeb45cda83d1
User Name <user@email.com>
2025-04-23 22:35:11 -0400

    documenting all Paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat_index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat_detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat_update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat_delete'),
+    path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('toys/', views.ToyList.as_view(), name='toy_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys-create'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
+
